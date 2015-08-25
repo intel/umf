60e0d1f0c0d502246b591ea6c8f69c0e2e6567d9
Harry Wang <hwang@zapsurgical.com>
2015-08-25 11:28:13 -0700

    Fixed compiling issue related to LICENSE. Fixed compiling issue in VS2015. Added new sample that demonstrates how to load schema from xml file.

---
+++ samples/metadata-schema/main.cpp
@@ -0,0 +1,97 @@
+// Copyright (C) 2014, Intel Corporation, all rights reserved.
+
+/*
+* This sample shows how to use predefined STD schema and built-in predefined metadata fields.
+*/
+
+#include <vmf/vmf.hpp>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+using namespace vmf;
+using namespace std;
+void dumpMetadataDesc(shared_ptr<MetadataDesc> spDesc)
+{
+	if (spDesc == nullptr)
+		return;
+
+	cout << "Metadata: " << spDesc->getMetadataName() << endl;
+	vector<FieldDesc> fields = spDesc->getFields();
+	for (FieldDesc field : fields)
+	{
+		cout << "\t[" << field.name << "]: " << Variant::typeToString(field.type) << std::endl;
+	}
+}
+void demoLoadSchemaFromFile(const string& schemaFilePath, const string& schemaName)
+{
+	MetadataStream stream;
+
+	try
+	{
+		// Open schema file
+		std::ifstream inputFile(schemaFilePath);
+
+		// Read schema content into memory string
+		stringstream ss;
+		ss << inputFile.rdbuf();
+
+		// Parse schema content and load it into stream
+		XMLReader reader;
+		stream.deserialize(ss.str(), reader);
+
+		// Get a pointer to the schema
+		shared_ptr<MetadataSchema> spSchema = stream.getSchema(schemaName);
+		if (spSchema == nullptr)
+			throw exception("Did not find schema in the xml file!");
+
+		// Get a pointer to the metadata description structure
+		shared_ptr<MetadataDesc> spMetadataDesc = spSchema->findMetadataDesc("table2");
+		if (spMetadataDesc == nullptr)
+			throw exception("Did not find metadata <table2> in the schema.");
+
+		// Found everything!
+		std::cout << "Found schema and metadata!" << std::endl;
+		std::cout << "Schema: " << spSchema->getName() << std::endl;
+		dumpMetadataDesc(spMetadataDesc);
+
+		stream.close();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Operation fialed: " << e.what() << std::endl;
+		stream.close();
+	}
+}
+
+int main(int argc, char* argv[])
+{
+    vmf::initialize();
+
+	string schemaFilePath("demo_schema.xml");
+	string schemaName("vmf://ns.intel.com/vmf/demo-schema-1.0");
+	
+	string appPath = argv[0];
+#ifdef WIN32
+	char delim = '\\';
+#else
+	char delim = '/';
+#endif
+	size_t pos = appPath.find_last_of(delim);
+
+	string workingPath;
+	if (pos != string::npos)
+	{
+		workingPath = appPath.substr(0, pos + 1);
+	}
+
+	// Run demo
+	demoLoadSchemaFromFile(workingPath + schemaFilePath, schemaName);
+
+    vmf::terminate();
+
+    return 0;
+}
