deadb5619b246a06c32e2bf4a82e65318f854675
Anna Plyakina <anna.plyakina@itseez.com>
2015-12-04 12:48:05 +0300

    IReader and IWriter interfaces + small fix for doxygen topic

---
+++ modules/vmfcore/java/IReader.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+interface IReader
+{
+	boolean parseAll (final String text, Long nextId,
+				   String filepath, String checksum,
+	               MetadataStream.VideoSegment[] segments, 
+				   MetadataSchema[] schemas,
+				   MetadataInternal[] metadata);
+				   
+	boolean parseSchemas (final String text, MetadataSchema[] schemas);
+	
+	boolean parseMetadata (final String text, final MetadataSchema[] schemas, MetadataInternal[] metadata);
+	
+	boolean parseVideoSegments(final String text, MetadataStream.VideoSegment[] segments);
+}
---
+++ modules/vmfcore/java/IWriter.java
@@ -0,0 +1,18 @@
+
+interface IWriter
+{
+    String store(final MetadataSchema[] schemas);
+
+    String store(final MetadataSet set);
+
+    String store(final Long nextId,
+                 final String filepath,
+                 final String checksum,
+                 final MetadataStream.VideoSegment[] segments,
+                 final MetadataSchema[] schemas,
+                 final MetadataSet set );
+
+    String store(final MetadataStream.VideoSegment spSegment);	
+
+    String store(final MetadataStream.VideoSegment[] segments)	
+}
