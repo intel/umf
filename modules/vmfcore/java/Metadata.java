928c677fe6ee9fbe1b6a198f665bb2c5ba918f63
Anna Plyakina <anna.plyakina@itseez.com>
2015-12-14 14:58:02 +0300

    changes for Metadata class

--- modules/vmfcore/java/Metadata.java
+++ modules/vmfcore/java/Metadata.java
@@ -8,6 +8,10 @@ public class Metadata
 	
 	protected long nativeObj;
 	
+	public static final long UNDEFINED_DURATION = 0;
+	
+	public static final long NUMOFFRAMES_DEFAULT = 1;
+	
 	protected Metadata (long addr)
     {
         if (addr == 0)
@@ -21,37 +25,116 @@ public class Metadata
 		nativeObj = n_Metadata (mdDescription.nativeObj);
 	}
 	
-	public void copy (Metadata Metadata)
+	public void copy (Metadata metadata)
 	{
-        n_copy (nativeObj, Metadata.nativeObj);
+		if ((nativeObj == 0) || (metadata.nativeObj == 0))
+			throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		n_copy (nativeObj, metadata.nativeObj);
 	}
 	
-	/*??public void clear ()
+	public void clear ()
 	{
-		if (nativeObj != 0)
-		    n_delete (nativeObj);
-		
-		nativeObj = 0;
-	}*/
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		    
+		n_clear (nativeObj);
+	}
 	
-	public long getId ()
+	public long getID ()
 	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
 		return n_getID (nativeObj);
 	}
 	
 	public long getFrameIndex ()
 	{
-		return;
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return n_getFrameIndex (nativeObj);
 	}
 	
 	public long getNumOfFrames ()
 	{
-		return;
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return n_getNumOfFrames (nativeObj);
 	}
 	
-	public void setFrameIndex ()
+	public void setFrameIndex (long frameIndex)
 	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
 		
+		n_setFrameIndex (nativeObj, frameIndex, NUMOFFRAMES_DEFAULT);
+	}
+	
+	public void setFrameIndexAndNumOfFrames (long frameIndex, long numOfFrames)
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		n_setFrameIndex (nativeObj, frameIndex, numOfFrames);
+	}
+	
+	public void setTimestamp (long time)
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		n_setTimestamp (nativeObj, UNDEFINED_DURATION);
+	}
+	
+	public void setTimestampAndDuration (long time, long duration)
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		n_setTimestamp (nativeObj, duration);
+	}
+	
+	public long getTimestamp ()
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return n_getTimestamp (nativeObj);
+	}
+	
+	public long getDuration ()
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return n_getDuration (nativeObj)
+	}
+	
+	public String getName ()
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return n_getName (nativeObj);
+	}
+	
+	public String getSchemaName ()
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return n_getSchemaName (nativeObj);
+	}
+	
+	public MetadataDesc getDesc ()
+	{
+		if (nativeObj == 0)
+		    throw new java.lang.UnsupportedOperationException("Native object address is NULL");
+		
+		return new MetadataDesc (n_getDesc (nativeObj));
 	}
 	
     @Override
