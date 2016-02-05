3ae53549a6f1bedba45b66d1f2cbb40fce80e696
Anna Plyakina <anna.plyakina@itseez.com>
2016-02-05 12:23:57 +0300

    add test suite

---
+++ modules/vmfcore/java/test/src/VmfTestSuite.java
@@ -0,0 +1,23 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  VmfFieldDescTest.class,
+  VmfFieldValueTest.class,
+  VmfJSONTest.class,
+  VmfLogTest.class,
+  VmfMetadataDescTest.class,
+  VmfMetadataSchemaTest.class,
+  VmfMetadataSetTest.class,
+  VmfMetadataStreamTest.class,
+  VmfMetadataTest.class,
+  VmfReferenceDescTest.class,
+  VmfReferenceTest.class,
+  VmfVariantTest.class,
+  VmfVec2d3d4dTest.class,
+  VmfVideoSegmentTest.class,
+  VmfXMLTest.class
+})
+
+public class VmfTestSuite {}
