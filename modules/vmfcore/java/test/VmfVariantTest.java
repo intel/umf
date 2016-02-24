87e53f4871edbbf3354d4ddcc97f67c956bfdc93
Anna Plyakina <anna.plyakina@itseez.com>
2016-02-24 14:24:07 +0300

    add tests to VmfVariantTest

--- modules/vmfcore/java/test/VmfVariantTest.java
+++ modules/vmfcore/java/test/VmfVariantTest.java
@@ -9,7 +9,9 @@ import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class VmfVariantTest 
 {
@@ -38,9 +40,10 @@ public class VmfVariantTest
     @Test
     public void testSetTo()
     {
-        float value1 = (float) 2.5;
-        int value2 = 4;
-        double value3 = 3.14;
+        float floatValue = 2.5f;
+        int intValue = 4;
+        double doubleValue = 3.14;
+        long longValue = 333333333;
         
         String str = "ok";
         
@@ -50,14 +53,17 @@ public class VmfVariantTest
         
         assertEquals(Variant.type_unknown, var1.getType());
         
-        var1.setTo(value1);
+        var1.setTo(floatValue);
         assertEquals(Variant.type_real, var1.getType());
         
-        var1.setTo(value2);
+        var1.setTo(intValue);
         assertEquals(Variant.type_integer, var1.getType());
         
-        var1.setTo(value3);
+        var1.setTo(doubleValue);
         assertEquals(Variant.type_real, var1.getType());
+        
+        var1.setTo(longValue);
+        assertEquals(Variant.type_integer, var1.getType());
         
         var1.setTo(str);
         assertEquals(Variant.type_string, var1.getType());
@@ -79,19 +85,35 @@ public class VmfVariantTest
         var1.setTo(buf);
         assertEquals(Variant.type_rawbuffer, var1.getType());
         
-        double array[] = new double [2];
-        array[0] = 0.0;
-        array[1] = 0.0;
+        long lArray[] = new long [2];
+        lArray[0] = 0;
+        lArray[1] = 0;
+        var1.setTo(lArray);
+        assertEquals(Variant.type_integer_vector, var1.getType());
         
-        var1.setTo(array);
+        double dArray[] = new double [2];
+        dArray[0] = 0.0;
+        dArray[1] = 0.0;
+        var1.setTo(dArray);
         assertEquals(Variant.type_real_vector, var1.getType());
         
         String strArray[] = new String [2];
         strArray[0] = "";
         strArray[1] = "";
-        
         var1.setTo(strArray);
         assertEquals(Variant.type_string_vector, var1.getType());
+        
+        float fArray[] = new float [2];
+        fArray[0] = 0.f;
+        fArray[1] = 0.f;
+        var1.setTo(fArray);
+        assertEquals(Variant.type_real_vector, var1.getType());
+        
+        int iArray[] = new int [2];
+        iArray[0] = 0;
+        iArray[1] = 0;
+        var1.setTo(iArray);
+        assertEquals(Variant.type_integer_vector, var1.getType());
         
         vmf_vec2d vec2dArray[] = new vmf_vec2d [2];
         vmf_vec3d vec3dArray[] = new vmf_vec3d [2];
@@ -122,5 +144,78 @@ public class VmfVariantTest
         assertTrue(var1.isEmpty());
         
         assertEquals("vec4d[]", var2.getTypeName());
-     }
+        
+        var2.setTo(longValue);
+        String value = var2.toString();
+        assertFalse(value.isEmpty());
+        
+        var2.fromString(Variant.type_integer, value);
+        
+        var2.fromString(Variant.type_real, "5.5");
+        assertEquals(var2.getType(), Variant.type_real);
+        value = var2.toString();
+        assertEquals("5.5", value);
+        
+        String typeStr = Variant.typeToString(Variant.type_vec4d_vector);
+        assertEquals ("vec4d[]", typeStr);
+        
+        int type = Variant.typeFromString(typeStr);
+        assertEquals(Variant.type_vec4d_vector, type);
+    }
+
+    @Test
+    public void testToAndFromString()
+    {
+        long longValue = 333333333;
+
+        var2.setTo(longValue);
+        String value = var2.toString();
+        assertFalse(value.isEmpty());
+
+        var2.fromString(Variant.type_integer, value);
+
+        var2.fromString(Variant.type_real, "5.5");
+        assertEquals(var2.getType(), Variant.type_real);
+
+        value = var2.toString();
+        assertEquals("5.5", value);
+
+        String typeStr = Variant.typeToString(Variant.type_vec4d_vector);
+        assertEquals ("vec4d[]", typeStr);
+
+        int type = Variant.typeFromString(typeStr);
+        assertEquals(Variant.type_vec4d_vector, type);
+
+        assertTrue(Variant.isConvertible(Variant.type_integer, Variant.type_real));
+        assertFalse(Variant.isConvertible(Variant.type_integer, Variant.type_string));
+
+        String base64Str = "trampampampampam";
+        byte[] buf = Variant.base64Decode(base64Str);
+        assertNotEquals(0, buf.length);
+
+        assertEquals(base64Str, Variant.base64Encode(buf));
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testConvertToThrown ()
+    {
+        long longValue = 555555;
+        var1.setTo(longValue);
+        thrown.expect(com.intel.vmf.VmfException.class);
+        thrown.expectMessage("vmf::Exception: Non-existent type.");
+        var1.convertTo (14);
+    }
+
+    @Test
+    public void testConvertToStringThrown ()
+    {
+        long longValue = 555555;
+        var1.setTo(longValue);
+        thrown.expect(com.intel.vmf.VmfException.class);
+        thrown.expectMessage("vmf::Exception: Cannot convert value to the target type!");
+        var1.convertTo (Variant.type_string);
+    }
 }
